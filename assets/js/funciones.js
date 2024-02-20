5d0f957dad989a6f1973a7c6a196ee807a7c74f8
Fernando05-blip <108412468+Fernando05-blip@users.noreply.github.com>
2024-02-20 17:51:20 -0300

    Update funciones.js

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,6 +1,6 @@
         const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
         const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl)) 
-
+;
         $(window).scroll(function () { 
     if ($("#header").offset().top >60){
         $("#header").addClass("bg-color");
